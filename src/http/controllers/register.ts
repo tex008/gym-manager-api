3dfcc236c4124fbaaf5533a0d27d782ccf205faf
tex008 <texera009@gmail.com>
2024-01-15 09:15:41 -0300

    feat: implements the repository interface

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '../usecases/register'
-import { PrismaUsersRepository } from './../repositories/prisma-users-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
--- src/http/usecases/register.ts
+++ src/http/usecases/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
+import { UsersRepository } from '../repositories/user-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,16 +9,12 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const passwordHash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('Email already exists.')
