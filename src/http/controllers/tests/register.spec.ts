7a2519a1c8bc432251ddf254b9abd4e50995d40f
tex008 <texera009@gmail.com>
2024-01-22 17:05:16 -0300

    feat: create register test

--- src/http/controllers/tests/register.spec.ts
+++ src/http/controllers/tests/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import requests from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await requests(app.server).post('/users').send({
+      name: 'Jonh Doe',
+      email: 'jonhdoe@test.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
